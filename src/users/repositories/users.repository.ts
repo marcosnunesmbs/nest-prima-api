f867b6c7484446a302ad3f4f8a3ab1b57ee025b4
Marcos Nunes <marcosnunesmbs@gmail.com>
2022-03-05 16:09:55 -0300

    CRUD posts

--- src/users/repositories/users.repository.ts
+++ src/users/repositories/users.repository.ts
@@ -15,13 +15,31 @@ export class UsersRepository {
   }
 
   async findAll(): Promise<UserEntity[]> {
-    return this.prisma.user.findMany();
+    return this.prisma.user.findMany({
+      include: {
+        posts: {
+          select: {
+            id: true,
+            title: true,
+            created_at: true,
+          },
+        },
+      },
+    });
   }
 
   async findOne(id: number): Promise<UserEntity> {
     return this.prisma.user.findUnique({
       where: {
         id,
+      },
+      include: {
+        posts: {
+          select: {
+            id: true,
+            title: true,
+          },
+        },
       },
     });
   }
@@ -30,6 +48,15 @@ export class UsersRepository {
     return this.prisma.user.update({
       where: { id },
       data: updateUserDto,
+      include: {
+        posts: {
+          select: {
+            id: true,
+            title: true,
+            created_at: true,
+          },
+        },
+      },
     });
   }
 
